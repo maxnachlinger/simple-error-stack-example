1352a0dfe3814430dace6ae071f930b36209d496
Max Nachlinger <max.nachlinger@gmail.com>
2014-08-17 13:42:53 -0700

    Fancy!

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('./iAteYourStack')(printError.bind(printError, 'iAteYourStack'));
 
-//require('./hereIsYourStack')(printError.bind(printError, 'hereIsYourStack'));
-//
-//require('./moreStack')(printError.bind(printError, 'moreStack'));
+require('./hereIsYourStack')(printError.bind(printError, 'hereIsYourStack'));
+
+require('./moreStack')(printError.bind(printError, 'moreStack'));
 
 function printError(msg, err) {
 	console.log('\n' + msg + '\n', err.stack);
